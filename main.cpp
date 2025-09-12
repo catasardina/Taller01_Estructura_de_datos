2b498c9c539d151b8dbce0b5c2909445cbfc893b
catasardina <mcdladr@gmail.com>
2025-09-12 11:56:47 -0300

    avances en el menu
    Gestion de notas e inscripciones

--- main.cpp
+++ main.cpp
@@ -21,12 +21,58 @@ int main(){
 
         switch (op)
         {
-        case 1:
-            cout
-            break;
-        
-        default:
+        case 1:{
+            int op;
+            cout << "Bienvenido a gestion de Alumnos" endl;
             break;
         }
-    }
+        case 2:
+            break;
+        case 3:
+            cout<< "Bienvenido a Gestion de inscripciones";
+            string id;
+            string codigo;
+            cout<< "Ingrese id alumno: "<< endl;
+            cin >> id;
+            cout<< "Ingrese codigo del curso: ";
+            cin>> codigo;
+            Alumno* alumno = listaAlumnos.buscarAlumnoId(id);
+            Curso* curso=listacursos.BuscarXCodigo(codigo);
+            if(alumno && curso){
+                if(a->getCarrera() == c->getCarrera()){
+                    listaInscripciones.addAlumno(alumno,curso);
+                }else{
+                    cout<<"Error";
+                }
+            }else{
+                cout<<"EL alumno o el curso no son validos" endl;
+            }
+            break;
+        case 4: {
+            cout<<" GESTION DE NOTAS" endl;
+            string id;
+            string codigo;
+            float nota;
+            cout<<"Ingrese id alumno: ";
+            cin>> id;
+            cout<<"Ingrese Codigo de curso: ";
+            cin>> codigo;
+            cout<< "Ingrese la nota: ";
+            cin>>nota;
+            listaInscripciones.addNota(id,codigo,nota);
+            break;
+        }
+        case 5:{
+            break;
+
+        }
+        case 0:{
+            cout <<"Has salido del sistema chao"<< endl;
+            break;
+        }
+        default:
+            cout<< "Opcion no valida reintente" endl;
+        }
+    } while(op!=0);
+    return 0;
 }
